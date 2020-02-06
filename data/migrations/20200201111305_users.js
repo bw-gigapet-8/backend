be242f629c5620fd26489e050e94f2673fdfe6c6
Cole Gonzales <colegonzales@Coles-MBP.attlocal.net>
2020-02-06 17:20:16 -0600

    commit

--- data/migrations/20200201111305_users.js
+++ data/migrations/20200201111305_users.js
@@ -2,7 +2,6 @@
 exports.up = function(knex) {
     return knex.schema.createTable('Users', (table) => {
         table.increments('id')
-            .notNullable()
         table.string('username')
             .notNullable()
         table.string('password')
@@ -10,7 +9,6 @@ exports.up = function(knex) {
         table.integer('pet_id')
             .references('id')
             .inTable('Pets')
-        table.string('token')
         table.boolean('profileCompleted')
             .notNullable()
             .defaultTo(0)
--- data/seeds/05_Users.js
+++ data/seeds/05_Users.js
@@ -6,9 +6,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('Users').insert([
-        {id: 1, username: 'Cole', pet_id: 2, password: bcrypt.hashSync('password1', 10), token: 'fo3h435u98435nc47096n69702362876c08924576928c'},
-        {id: 2, username: 'Brittany', pet_id: 3, password: bcrypt.hashSync('password2', 10), token: '86029h45703fh6oi354gh7po3h457h3567g3hg73'},
-        {id: 3, username: 'Tyler', pet_id: 4, password: bcrypt.hashSync('password3', 10), token: 'k4h6l435h54kj73l57b54lk73blu573n5k7b3kb6'}
+        {id: 1, username: 'Cole', pet_id: 2, password: bcrypt.hashSync('password1', 10)},
+        {id: 2, username: 'Brittany', pet_id: 3, password: bcrypt.hashSync('password2', 10)},
+        {id: 3, username: 'Tyler', pet_id: 4, password: bcrypt.hashSync('password3', 10)}
       ]);
     });
 };
