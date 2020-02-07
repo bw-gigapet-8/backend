dbf6fec8d1ca5a74ccc614182932ccd5d6f2742a
Cole Gonzales <colegonzales@Coles-MBP.attlocal.net>
2020-02-07 15:32:50 -0600

    fdsagfad

--- utils/helpers.js
+++ utils/helpers.js
@@ -92,7 +92,7 @@ async function updateFoodEntry(pet_id, food_eaten_id, changes) {
 async function addFood(food) {
     console.log(`FOOD`, food)
     const foodAdded = await db('Foods').insert({ name: food.name, category_id: food.category_id }).returning("*")
-    console.log(`addFood ID`, foodAdded)
+    console.log(`addFood ID`, foodAdded[0])
     return foodAdded[0]
 }
 
