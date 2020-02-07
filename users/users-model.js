09640b59b0fc579619c48343f8bef5e5d8d6411e
Cole Gonzales <colegonzales@Coles-MBP.attlocal.net>
2020-02-06 20:39:56 -0600

    commit

--- users/users-model.js
+++ users/users-model.js
@@ -26,8 +26,8 @@ async function getAllUsers() {
 }
 
 async function findById(id) {
-    const user = await db('Users').where({ id }).first().select('id', 'username', 'password', 'pet_id')
-    return user
+    const user = await db('Users').where({ id }).select('id', 'username', 'password', 'pet_id')
+    return user[0]
 }
 
 async function findUsersPet(user_id) {
