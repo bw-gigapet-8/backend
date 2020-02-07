4a0eabcd199aeeb1484ab39811320b2cf2e24a28
Cole Gonzales <colegonzales@Coles-MBP.attlocal.net>
2020-02-06 19:23:30 -0600

    commit

--- pets/pets-model.js
+++ pets/pets-model.js
@@ -10,7 +10,7 @@ async function createPet(req, res, pet_data) { // Requires a pet_name
     try {
         const pet = await db('Pets').insert(pet_data)
         const newPet = await findPet(pet)
-        console.log(newPet)
+        console.log(newPet[0])
         const pet_id = newPet.id
         await db('Users').where({id: req.params.id}).update({pet_id: pet_id})
         return newPet
