085f54566cf6b555d980d2ee599f3a3cc9f03333
Cole Gonzales <colegonzales@Coles-MBP.attlocal.net>
2020-02-07 16:23:00 -0600

    fd

--- data/seeds/04_Foods_Eaten.js
+++ data/seeds/04_Foods_Eaten.js
@@ -5,10 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('Foods_Eaten').insert([
-        {id: 1, pet_id: 3, food_id: 2, time_of_day: 'Breakfast'},
-        {id: 2, pet_id: 1, food_id: 2, time_of_day: 'Breakfast'},
-        {id: 3, pet_id: 1, food_id: 3, time_of_day: 'Lunch'},
-        {id: 4, pet_id: 2, food_id: 1, time_of_day: 'Dinner'}
+        {id: 1, pet_id: 3, food_id: 2},
+        {id: 2, pet_id: 1, food_id: 2},
+        {id: 3, pet_id: 1, food_id: 3},
+        {id: 4, pet_id: 2, food_id: 1}
       ]);
     });
 };
